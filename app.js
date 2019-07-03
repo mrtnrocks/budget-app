2874339ce2929b722fbc543f36e56d4267e851fa
mrtn.codes <mrtn.glch@tuta.io>
2019-07-03 11:51:18 +0200

    6.7

--- app.js
+++ app.js
@@ -10,18 +10,38 @@ var budgetController = (function() {
     // UI CONTROLLER
 var UIController = (function() {
 
-    // Some code
+    var DOMstrings ={
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
 
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either in or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }    
 })(); 
 
 
     // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctlrAddItem = function() {
                 
         // 1. Get the field input data
-
+        var input = UICtrl.getInput();
+        console.log(input);
         // 2. Add the item to the budget controller
 
         // 3. Add the item to the UI
@@ -29,10 +49,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-        console.log('work');
+        
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctlrAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctlrAddItem);
 
     document.addEventListener('keypress', function(event) {
         if(event.keyCode === 13 || event.which === 13) {
