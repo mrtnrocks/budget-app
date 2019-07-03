ca21fdd90cea10e97eb6e07901b7232f6330e7b2
mrtn.codes <mrtn.glch@tuta.io>
2019-07-03 11:12:22 +0200

    6.6

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
+
+    // BUDGET CONTROLLER
 var budgetController = (function() {
 
-    var x = 23;
-
-    var add = function(a) {
-        return x + a; 
-    }
-
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
+ 
 
 })();
 
 
+    // UI CONTROLLER
 var UIController = (function() {
 
     // Some code
@@ -22,14 +15,32 @@ var UIController = (function() {
 })(); 
 
 
+    // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
+    var ctlrAddItem = function() {
+                
+        // 1. Get the field input data
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+        console.log('work');
     }
 
+    document.querySelector('.add__btn').addEventListener('click', ctlrAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if(event.keyCode === 13 || event.which === 13) {
+            
+            ctlrAddItem();
+
+        }
+    });
+
 })(budgetController, UIController);
+
