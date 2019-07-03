a4b78a1c78a703e4f002027f6a57d47a8aa1f95d
mrtn.codes <mrtn.glch@tuta.io>
2019-07-03 10:20:10 +0200

    2.5

---
+++ app.js
@@ -0,0 +1,35 @@
+var budgetController = (function() {
+
+    var x = 23;
+
+    var add = function(a) {
+        return x + a; 
+    }
+
+    return {
+        publicTest: function(b){
+            return add(b);
+        }
+    }
+
+})();
+
+
+var UIController = (function() {
+
+    // Some code
+
+})(); 
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
