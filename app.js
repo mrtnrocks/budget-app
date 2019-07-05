07487bf5efbdb9117e8958f43d3411a07963c7af
mrtn.codes <mrtn.glch@tuta.io>
2019-07-05 11:05:23 +0200

    6.12 done

--- app.js
+++ app.js
@@ -109,7 +109,22 @@ var UIController = (function() {
 
 
         }, 
- 
+        
+        clearFields() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            // Problem: fields is a List --> we have to turn it into an Array to apply Array Methods
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = '';
+            });
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -141,9 +156,11 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItems(input.type, input.description, input.value);
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
-        // 4. Calculate the budget
+        // 4. Clear the Fields
+        UICtrl.clearFields();
+        // 5. Calculate the budget
 
-        // 5. Display the budget on the UI
+        // 6. Display the budget on the UI
         
     };
 
