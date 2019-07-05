69a2e4f00666c236ffedebbcf4a5b5d5bea39267
mrtn.codes <mrtn.glch@tuta.io>
2019-07-05 16:33:01 +0200

    6.15 done

--- app.js
+++ app.js
@@ -106,7 +106,11 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -159,6 +163,20 @@ var UIController = (function() {
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+            
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -187,7 +205,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 2. Return the Budget
         var budget = budgetCtrl.getBudget();
         // 3. Display the budget on the UI
-        console.log(budget); 
+        UICtrl.displayBudget(budget);
     };
 
     var ctrlAddItem = function() {
@@ -214,7 +232,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     return {
         init: function() {
-            console.log('Application has started')
+            console.log('Application has started');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     }
